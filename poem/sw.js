fa96581b088bf0641d8709ee155d73b990b86660
shuxiang <703647752@qq.com>
2024-05-14 17:51:26 +0800

    fix

--- poem/sw.js
+++ poem/sw.js
@@ -2,11 +2,11 @@ const CACHE_NAME = 'cool-cache';
 
 // Add whichever assets you want to precache here:
 const PRECACHE_ASSETS = [
-    '/css/',
-    '/fonts/',
-    '/js/',
-    '/icons/',
-    '/static/'
+    './css/',
+    './fonts/',
+    './js/',
+    './icons/',
+    './static/'
 ]
 
 // Listener for the install event - precaches our assets list on service worker install.
